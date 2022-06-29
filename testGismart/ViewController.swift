872432454d50ff990dec34bf02ff6e559bd6b50c
Mikalai Zhdanko <mikalai.zhdanko@innowise-group.com>
2022-06-29 22:12:04 +0300

    initial commit

---
+++ testGismart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGismart
+//
+//  Created by Николай on 29.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
